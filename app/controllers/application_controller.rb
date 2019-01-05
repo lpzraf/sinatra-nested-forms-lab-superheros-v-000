d449329dc8a71ff4863f9788d71935204f3cefea
Rafael Lopez <lpz.raf@gmail.com>
2019-01-05 21:53:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
     post '/team' do
         #binding.pry
         @team = params[:team][:name],params[:team][:motto]
-        @member1 = params[:team][:name],params[:team][:motto]
-        @member2 =
-        @member3 = 
+        @member1 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
+        @member2 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
+        @member3 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
   
       erb :team
     end
