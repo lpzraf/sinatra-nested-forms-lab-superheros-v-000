dd799325103971f8422643a415645298cc94b754
Rafael Lopez <lpz.raf@gmail.com>
2019-01-05 21:32:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
+require "pry"
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
-
-
+    
+    get '/' do 
+      
+      erb :super_hero
+    end
+    
+    post '/teams' do
+        @team = Team.new(params[:team][:name],params[:team][:motto])
+  
+        # @first_ship = Ship.new(params[:pirate][:ships][0])
+        # @second_ship = Ship.new(params[:pirate][:ships][1])
+      #erb :
+    end
 end
