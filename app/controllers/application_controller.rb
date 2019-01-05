a2751d5e6d250a4adec632db00ad0025a476127f
Rafael Lopez <lpz.raf@gmail.com>
2019-01-05 22:01:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
     
     post '/team' do
         binding.pry
-        @team = params[:team][:name]
+        @team = params[:team]
         @member1 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
         @member2 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
         @member3 = params[:team][:hero][:name], params[:team][:hero][:power], params[:team][:hero][:biography]
